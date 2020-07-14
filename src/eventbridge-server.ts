ee90ed2b6df33fc487a9358da2291f46a9aada6d
Mike Christofersen <mike.christofersen@vouch.us>
2020-07-14 10:48:12 -0500

    fix issue with the server dropping when handlers failed

--- src/eventbridge-server.ts
+++ src/eventbridge-server.ts
@@ -71,7 +71,7 @@ export class EventBridgeServer {
                 FunctionName: rule.function,
                 InvocationType: 'RequestResponse',
                 Payload: JSON.stringify(payload),
-            }).promise().then(res => ({...JSON.parse(res.$response.httpResponse.body.toString()), function: rule.function}))
+            }).promise().then(res => ({...JSON.parse(res.$response.httpResponse.body.toString()), function: rule.function})).catch(err => err)
         }))
     }
 
